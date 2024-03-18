30c44e0428819bbf6160566f97f0c08891078444
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-18 09:45:13 +0100

    Ajout de la deuxième ranger intégrant les boutons pour entrer dans l'application
     → Ajout du bouton pour entrer dans le menu et de son style
    
     → Ajout du bouton pour se connecter ou s'enregistrer et de son style
    
     → Modification du margin de 5% à 6% à gauche et à droite des rangers

--- src/components/home/SNavbar.vue
+++ src/components/home/SNavbar.vue
@@ -2,14 +2,39 @@
 </script>
 
 <template>
+  <!--
+      Barre de navigation pour la landing page.
+   -->
   <nav class="row s-navbar">
+
+    <!-- Première ranger contenant le logo & les hypertextes -->
     <div class="row">
+
+      <!-- div pour le logo -->
       <div class="row" id="logo">
+        <!-- TODO: Mettre le logo -->
         <h3>PlayTrays</h3>
       </div>
+
+      <!-- hypertextes -->
       <a>About</a>
       <a>Games</a>
       <a>Contact</a>
+
+    </div>
+
+    <!-- Deuxième ranger contenant les boutons pour entrer dans l'application -->
+    <div class="row">
+
+      <!-- bouton pour aller au menu -->
+      <button class="play-button">
+        Play on a Tray!
+      </button>
+
+      <!-- bouton pour se connecter ou s'enregistrer -->
+      <button class="sign-up-button">
+        Sign up
+      </button>
     </div>
   </nav>
 </template>
@@ -19,19 +44,22 @@
     width: 100%;
     height: 200px;
     align-items: center;
+    justify-content: space-between;
   }
 
-  /* style de la rangé du logo et des hypertextes */
+
+
+  /* style de la première rangé du logo et des hypertextes */
   .s-navbar>div:nth-child(1) {
-    margin-left: 5%;
+    margin-left: 6%;
     width: 47.5%;
     align-items: center;
     justify-items: center;
     justify-content: space-between;
   }
 
-  /* style des hypertextes de la SNavbar */
-  .s-navbar a {
+  /* style des hypertextes */
+  .s-navbar>div:nth-child(1)>a {
     font-size: 32px;
     font-weight: 300;
     color:rgb(var(--link-color));
@@ -46,5 +74,30 @@
   #logo>h3 {
     font-size: 48px;
     font-weight: 800;
+  }
+
+  /* style de la seconde rangé  */
+  .s-navbar>div:nth-child(2) {
+    width: 30%;
+    margin-right: 6%;
+    align-items: center;
+    justify-items: center;
+    justify-content: space-evenly;
+  }
+
+  /* style du bouton pour entrer dans le menu */
+  .play-button {
+    width: 300px;
+    height: 60px;
+    border-color: rgb(var(--primary-color));
+  }
+
+
+  /* style du bouton pour se connecter ou s'enregistrer */
+  .sign-up-button {
+    width: 200px;
+    height: 60px;
+    border-color: rgb(var(--text-color));
+    background-color: transparent;
   }
 </style>
