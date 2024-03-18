afbbecda3c3a48e00d936edfdff844dd65e58ad3
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-18 08:35:40 +0100

    Ajout du style pour la barre de navigation & de la première ranger (logo suivi des hypertextes)

--- src/components/home/SNavbar.vue
+++ src/components/home/SNavbar.vue
@@ -4,21 +4,47 @@
 <template>
   <nav class="row s-navbar">
     <div class="row">
-      <div id="logo">
+      <div class="row" id="logo">
         <h3>PlayTrays</h3>
       </div>
-      <h3>About</h3>
+      <a>About</a>
+      <a>Games</a>
+      <a>Contact</a>
     </div>
   </nav>
 </template>
 
 <style scoped>
   .s-navbar {
-    width: 100vw;
-    margin: 2.5% 5%;
+    width: 100%;
+    height: 200px;
+    align-items: center;
   }
 
+  /* style de la rangé du logo et des hypertextes */
   .s-navbar>div:nth-child(1) {
+    margin-left: 5%;
+    width: 47.5%;
+    align-items: center;
+    justify-items: center;
+    justify-content: space-between;
+  }
 
+  /* style des hypertextes de la SNavbar */
+  .s-navbar a {
+    font-size: 32px;
+    font-weight: 300;
+    color:rgb(var(--link-color));
+  }
+
+  /* style du logo */
+  #logo {
+    padding-bottom: 1%;
+  }
+
+  /* style de la police du logo */
+  #logo>h3 {
+    font-size: 48px;
+    font-weight: 800;
   }
 </style>
