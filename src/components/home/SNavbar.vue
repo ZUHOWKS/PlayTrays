a9ae11c8c79a23c11d47739dc150a9ef138af173
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-18 08:12:35 +0100

    Changement d'emplacement pour `SNavbar.vue', déplacer de `showcase/` à `home/`

---
+++ src/components/home/SNavbar.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <nav class="row s-navbar">
+    <div class="row">
+      <div id="logo">
+        <h3>PlayTrays</h3>
+      </div>
+      <h3>About</h3>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+  .s-navbar {
+    width: 100vw;
+    margin: 2.5% 5%;
+  }
+
+  .s-navbar>div:nth-child(1) {
+
+  }
+</style>
