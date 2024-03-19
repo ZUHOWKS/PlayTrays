55994cdff4088d5c4a0cee9eabbb3fd388ec7252
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-19 14:18:37 +0100

    Création du composant `Welcome.vue` pour la section de bienvenu

---
+++ src/components/home/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="row welcome-section">
+    <h1>Welcome</h1>
+  </section>
+
+</template>
+
+<style scoped>
+  .welcome-section {
+    width: 100vw;
+    height: 70vh;
+    justify-items: center;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+</style>
