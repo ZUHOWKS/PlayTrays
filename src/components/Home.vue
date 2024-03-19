27b8709419fd8e0a4f02a62767ec32cd9441cb8c
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-19 14:20:33 +0100

    Intégration du composant `Welcome.vue` dans la balise main.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import SNavbar from "@/components/home/SNavbar.vue";
+import Welcome from "@/components/home/Welcome.vue";
 </script>
 
 <template>
   <SNavbar></SNavbar>
+  <main>
+    <Welcome></Welcome>
+  </main>
 </template>
 
 <style scoped>
