4ada4329657fd2df8ccbd57561795f3973ee4bef
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-21 03:51:00 +0100

    Ajout de commentaires

--- src/modules/game/TrayGame.ts
+++ src/modules/game/TrayGame.ts
@@ -7,6 +7,12 @@ export default class TrayGame {
     players: string[];
     sceneController: SceneController;
 
+    /**
+     *
+     * @param id id ou uuid de la partie
+     * @param statement statut de la partie ("en attente de joueurs" | "en cours" | "terminée")
+     * @param sceneController contrôleur de la vue 3D
+     */
     constructor(id: string, statement: "waiting" | "running" | "finished", sceneController: SceneController) {
         this.id = id;
         this.statement = statement;
