ac3af0c415caa16d23f2b104c7ee560cd5ec1a98
Majurax <legeekmajurax@gmail.com>
2024-03-21 16:29:08 +0100

    Stealing of my work by Joris Villardel

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,11 @@
+// import type { HttpContext } from '@adonisjs/core/http'
+
+export default class AuthController {
+  private async register() {
+    return this.register.name
+  }
+
+  private async login() {
+    return this.login.name
+  }
+}
