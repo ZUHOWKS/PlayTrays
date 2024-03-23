b03992f0fb7cfe7f1a63e58583d403ab917f458e
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-23 17:15:16 +0100

    Modification des attributs de la classe TrayGame:
     → Liste des joueurs remplacée par le joueur client
     → sceneController remplacé par controller du type SupportController

--- src/modules/game/TrayGame.ts
+++ src/modules/game/TrayGame.ts
@@ -1,22 +1,25 @@
-import type SceneController from "@/modules/game/SceneController";
+import SupportController from "@/modules/game/scene/SupportController";
 
 
 export default class TrayGame {
     id: string;
     statement: "waiting" | "running" | "finished";
-    players: string[];
-    sceneController: SceneController;
+    player: any;
+    controller: SupportController;
 
     /**
+     * Cette classe facilite le rassemblement entre contrôleur de partie
+     * et données utilisateurs
      *
      * @param id id ou uuid de la partie
      * @param statement statut de la partie ("en attente de joueurs" | "en cours" | "terminée")
-     * @param sceneController contrôleur de la vue 3D
+     * @param player données de l'utilisateur
+     * @param controller contrôleur de la partie
      */
-    constructor(id: string, statement: "waiting" | "running" | "finished", sceneController: SceneController) {
+    constructor(id: string, statement: "waiting" | "running" | "finished", player: any, controller: SupportController) {
         this.id = id;
         this.statement = statement;
-        this.players = [];
-        this.sceneController = sceneController;
+        this.player = player; //TODO: remplacer any par la User class
+        this.controller = controller;
     }
 }
