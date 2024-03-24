535ee4b0c1d6567a3d25a5579bd18fceb24f62a9
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-24 01:50:58 +0100

    Création de la classe `CheckersClient` contrôleur de la scène pour le jeu des dames

---
+++ src/modules/game/scene/controllers/CheckersClient.ts
@@ -0,0 +1,42 @@
+import SupportController from "@/modules/game/scene/SupportController";
+import type Actuator from "@/modules/game/scene/actionners/Actuator";
+import type PTObject from "@/modules/game/scene/objects/PTObject";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+import Tray from "@/modules/game/scene/objects/Tray";
+import {PerspectiveCamera, Scene} from "three";
+import type {Ref} from "vue";
+import type {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+
+
+export default class CheckersClient extends SupportController {
+
+    constructor(scene: Scene, cameraRef: Ref<PerspectiveCamera>, orbitControlsRef: Ref<OrbitControls | null>) {
+        super(scene, cameraRef, orbitControlsRef);
+    }
+
+    confirmAction(): void {
+    }
+
+    getSelectedActuator(): Actuator | null {
+        return null;
+    }
+
+    selectActuator(name: string): void {
+    }
+
+    setup(): void {
+        const loader = new GLTFLoader();
+
+        this.loadGLTFSceneModel(loader, "checkers/checkers_tray.glb").then((obj) => {
+            obj.name = "checkersTray";
+            this.registerObject(new Tray(obj.name, obj));
+        });
+    }
+
+    showSelectedObjectActuators(object: PTObject | null): void {
+    }
+
+    unselectAll(): void {
+    }
+
+}
