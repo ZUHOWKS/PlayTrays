3777fe39d50bc69843261f5ea78c9a498ffab6ac
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-24 01:47:50 +0100

    Création de la classe `Tray` pour les plateaux

---
+++ src/modules/game/scene/objects/Tray.ts
@@ -0,0 +1,23 @@
+
+import PTObject from "@/modules/game/scene/objects/PTObject";
+import type {Object3D} from "three";
+
+export default class Tray extends PTObject {
+
+    constructor(name: string, object3D: Object3D) {
+        super(name, object3D);
+    }
+
+    moveTo(x: number, y: number, z: number): void {
+    }
+
+    rotate(x: number, y: number, z: number): void {
+    }
+
+    select(): void {
+    }
+
+    unselect(): void {
+    }
+
+}
