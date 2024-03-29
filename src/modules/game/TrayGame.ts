ec6ac770ea62a54b23e1fde1eacf5cfc9c067320
ZUHOWKS <jojo.vilard38@gmail.com>
2024-03-29 01:50:05 +0100

    Ajout d'un nouvel attribut `ws` correspondant au socket en lien avec le serveur jeu & modification du constructeur

--- src/modules/game/TrayGame.ts
+++ src/modules/game/TrayGame.ts
@@ -1,11 +1,17 @@
 import SupportController from "@/modules/game/scene/SupportController";
+import {Socket} from "socket.io-client";
+import {PerspectiveCamera, Scene} from "three";
+import type {Ref} from "vue";
+import type {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+import CheckersClient from "@/modules/game/scene/controllers/CheckersClient";
 
 
 export default class TrayGame {
     id: string;
     statement: "waiting" | "running" | "finished";
     player: any;
-    controller: SupportController;
+    controller: SupportController | undefined;
+    ws: Socket;
 
     /**
      * Cette classe facilite le rassemblement entre contrôleur de partie
@@ -14,16 +20,29 @@ export default class TrayGame {
      * @param id id ou uuid de la partie
      * @param statement statut de la partie ("en attente de joueurs" | "en cours" | "terminée")
      * @param player données de l'utilisateur
-     * @param controller contrôleur de la partie
+     * @param ws
+     * @param game jeu de la partie
+     * @param scene Scene 3D
+     * @param cameraRef Caméra du joueur (vue du joueur)
+     * @param orbitControlsRef Contrôles orbitaux de la caméra (autour d'un point,
+     * par défaut le point de coordonnée x=0;y=0;z=0)
      */
-    constructor(id: string, statement: "waiting" | "running" | "finished", player: any, controller: SupportController) {
+    constructor(id: string, statement: "waiting" | "running" | "finished", player: any, ws: Socket, game: string, scene: Scene, cameraRef: Ref<PerspectiveCamera>, orbitControlsRef: Ref<OrbitControls | null>,) {
         this.id = id;
         this.statement = statement;
         this.player = player; //TODO: remplacer any par la User class
-        this.controller = controller;
+        this.ws = ws;
+
+        if (game == "checkers") {
+            this.controller = new CheckersClient(scene, cameraRef, orbitControlsRef, this.ws);
+        }
     }
 
     setup(): void {
-        this.controller.setup();
+        if (this.controller) {
+            this.controller.setup();
+        } else {
+            this.ws.disconnect();
+        }
     }
 }
