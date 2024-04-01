b52c30d816bd2e993d37b1cf9c527b6f4989e0b7
ZUHOWKS <jojo.vilard38@gmail.com>
2024-04-01 21:56:48 +0200

    Ajout d'un nouvel attribut `queen` pour savoir si un pion est devenu une reine.

--- src/modules/checkers/Pawn.ts
+++ src/modules/checkers/Pawn.ts
@@ -4,12 +4,15 @@ export default class Pawn {
     name: string;
     x: number;
     y: number;
+    z: number;
     dead: boolean = false;
+    queen: boolean = false;
 
 
-    constructor(name: string, x: number, y: number) {
+    constructor(name: string, x: number, y: number, z: number) {
         this.name = name;
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 }
