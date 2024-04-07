dc97c182817385203e3622939844ca7b4bb5c2fa
ZUHOWKS <jojo.vilard38@gmail.com>
2024-04-07 13:21:06 +0200

    Ajout de méthode pour synchroniser le jeu entre les utilisateurs :
    Détermine les équipes, anti-cheat (vérifie les actions des joueurs), synchronise la partie entre les clients.

--- src/modules/lobby/games/Checkers.ts
+++ src/modules/lobby/games/Checkers.ts
@@ -8,6 +8,7 @@ export default class Checkers extends PTLobby {
     pawns: Pawn[];
     whitePlayer: string | undefined;
     blackPlayer: string | undefined;
+    whoPlay: "black" | "white" = "black";
 
     /**
      * Permet d'instancier un lobby pour le jeu des dames.
@@ -52,18 +53,42 @@ export default class Checkers extends PTLobby {
         socket.join(this.uuid);
 
         // émettre une update de la partie
-        socket.emit("setup game", this.getPawnsJSON(), this.setTeam(socket.data.user));
+        const userTeam = this.setTeam(socket.data.user);
+        socket.emit("setup game", this.getPawnsJSON(), {team: userTeam, canPlay: this.whoPlay == userTeam});
 
         // évènement de déconnection du socket
-        socket.on("disconnect", (reason, description) => {
+        socket.on("disconnect", () => {
             this.removeSocket(socket.data.user);
             console.log("The user " + socket.data.user + " leave the lobby " + this.uuid + ".");
         });
 
+        socket.on("pawn action", (action: {
+            pawn: string,
+            moveX: number,
+            moveY: number,
+            moveZ: number,
+        }, callback) => {
+            const result: string | undefined = this.pawnMoveAction(action, socket);
+            const userTeam = this.getTeam(socket.data.user);
+            if (!result) {
+                return callback("Error: can't perform this action !", {pawns: this.getPawnsJSON(), team: userTeam, canPlay: this.whoPlay == userTeam});
+                //socket.emit("rollback game", this.getPawnsJSON())
+            } else {
+
+                return callback("", {pawnKilled: result == action.pawn ? undefined : result, team: userTeam, canPlay: this.whoPlay == userTeam});
+            }
+
+        })
 
         console.log("The user " + socket.data.user + " join the lobby " + this.uuid + ".")
     }
 
+    /**
+     * Place le joueur dans une équipe.
+     *
+     * @param user nom de l'utilisateur
+     * @private
+     */
     private setTeam(user: string): "white" | "black" | "spectator" {
         if (!this.blackPlayer || this.blackPlayer == user) {
             this.blackPlayer = user;
@@ -76,6 +101,99 @@ export default class Checkers extends PTLobby {
         }
     }
 
+    private getTeam(user: string): "white" | "black" | "spectator" {
+        if (this.blackPlayer == user) {
+            return "black";
+        } else if (this.whitePlayer == user) {
+            return "white";
+        } else {
+            return "spectator";
+        }
+    }
+
+    private pawnMoveAction(action: { pawn: string, moveX: number, moveY: number, moveZ: number, }, socket: Socket): string | undefined {
+        const pawn: Pawn | undefined = this.getPawn(action.pawn);
+        if (this.getTeam(socket.data.user) == this.whoPlay && Math.abs(action.moveX) <= 21 && Math.abs(action.moveZ) <= 21) {
+            if (pawn) {
+                if (Math.abs(action.moveY - pawn.y) == 0 && pawn.name.includes(this.getTeam(socket.data.user))) {
+                    const pos: {x: number, y: number, z: number} = {x: action.moveX, y: action.moveY, z: action.moveZ};
+
+                    const xDiff: number = action.moveX - pawn.x;
+                    const zDiff: number = action.moveZ - pawn.z;
+
+                    if (Math.abs(xDiff) == 6 && Math.abs(zDiff) == 6) {
+                        if (!this.anyPawnAt(pos)) {
+                            this.whoPlay = this.getTeam(socket.data.user) == "black" ? "white" : "black"
+                            pawn.setPosition(pos);
+                            this.emitWithout( socket, "pawn action", action, undefined)
+                            return pawn.name;
+                        }
+                    } else if (Math.abs(xDiff) == 12 && Math.abs(zDiff) == 12) {
+                        const pawnToKill: Pawn | undefined = this.anyPawnAt({x: pos.x + (xDiff < 0 ? 1 : -1) * 6, y: pos.y, z: pos.z + (zDiff < 0 ? 1 : -1) * 6});
+                        if (pawnToKill) {
+                            if (!pawnToKill.name.includes(pawn.name.split("-")[0])) {
+                                if (!this.anyPawnAt(pos)) {
+                                    this.whoPlay = this.getTeam(socket.data.user) == "black" ? "white" : "black"
+                                    pawn.setPosition(pos);
+                                    pawnToKill.dead = true;
+                                    this.emitWithout( socket, "pawn action", action, pawnToKill.name)
+                                    return pawnToKill.name;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return undefined;
+
+    }
+
+    /**
+     * Renvoie le pion du nom donné.
+     *
+     * @param name nom du pion recherché
+     * @return le pion, sinon undenfined
+     * @private
+     */
+    private getPawn(name: string): Pawn | undefined {
+
+        for (let i: number = 0; i < this.pawns.length; i++) {
+            const pawn: Pawn = this.pawns[i];
+            if (pawn.name == name) {
+                return pawn;
+            }
+        }
+
+        return;
+    }
+
+    /**
+     * Renvoie le pion trouvé à une localisation donnée.
+     *
+     * @param pos Localisation
+     * @return Le pion trouvé à la localisation donnée, sinon undefined.
+     * @private
+     */
+    private anyPawnAt(pos: {x: number; y: number; z: number}): Pawn | undefined {
+
+        for (let i:number = 0; i < this.pawns.length; i++) {
+            const pawn: Pawn = this.pawns[i];
+
+            if (pawn.x == pos.x && pawn.y == pos.y && pawn.z == pos.z && !(pawn.dead)) {
+                return pawn;
+            }
+        }
+
+        return;
+    }
+
+    /**
+     * Obtenir la liste des pions ainis que leurs attributs au format JSON.
+     *
+     * @private
+     */
     private getPawnsJSON(): { name: string; x: number; y: number; z: number; dead: boolean; queen: boolean}[] {
         const pawns: { name: string; x: number; y: number; z: number; dead: boolean; queen: boolean}[] = [];
 
@@ -89,5 +207,16 @@ export default class Checkers extends PTLobby {
         }))
 
         return pawns;
+    }
+
+
+    emitWithout(socketNotEmit: Socket, event: string, ...args: any[]) {
+        const socketArray: Socket[] = Array.from(this.sockets.values());
+        socketArray.forEach((socket) => {
+            if (socket.data.user != socketNotEmit.data.user) {
+                const userTeam = this.getTeam(socket.data.user)
+                socket.emit(event, ...args, {team: userTeam, canPlay: this.whoPlay == userTeam});
+            }
+        })
     }
 }
