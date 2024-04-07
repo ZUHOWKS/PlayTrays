1af5482d14aebb8fd7e4a5d5c35624167a918eda
ZUHOWKS <jojo.vilard38@gmail.com>
2024-04-07 13:16:21 +0200

    Ajout d'une méthode `setPosition()` par la nouvelle

--- src/modules/checkers/Pawn.ts
+++ src/modules/checkers/Pawn.ts
@@ -15,4 +15,15 @@ export default class Pawn {
         this.y = y;
         this.z = z;
     }
+
+    /**
+     * Remplace la position actuelle par la nouvelle position passée en paramètre
+     *
+     * @param pos vecteur à 3 coordonnées
+     */
+    setPosition(pos: {x: number, y: number, z: number}) {
+        this.x = pos.x;
+        this.y = pos.y;
+        this.z = pos.z;
+    }
 }
