abbc9a48a807f927eb977949114d630d10a89fa7
ZUHOWKS <jojo.vilard38@gmail.com>
2024-04-24 23:40:31 +0200

    Implémentation de vue-router + gestion de l'authentification

---
+++ src/views/Menu.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+  import {useRouter} from "vue-router";
+  import {type Ref, ref} from "vue";
+  import axios from "axios";
+  import type User from "@/modules/utils/User";
+
+  const router = useRouter();
+  if (!localStorage.getItem("PTToken")) router.push('/login');
+
+  axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("PTToken")}`;
+
+  const user: Ref<User> = ref({
+    id: -1,
+    username: '',
+    points: -1,
+    updatedAt: '',
+    createdAt: ''
+  })
+
+  function init() {
+    axios.get('http://localhost:3333/api/v1/user/info').then((response) => {
+      user.value.id = response.data.id;
+      user.value.username = response.data.username;
+      user.value.points = response.data.points;
+      user.value.updatedAt = response.data.updatedAt;
+      user.value.createdAt = response.data.createdAt;
+    })
+  }
+
+  init();
+
+  function logout() {
+    localStorage.removeItem("PTToken");
+    router.push('/');
+  }
+</script>
+
+<template>
+  <p>{{user.username}}</p>
+  <p>{{user.points}} points</p>
+
+  <button class="logout" @click="logout">
+    Logout
+  </button>
+</template>
+
+<style scoped>
+
+</style>
