0f93bbae2a15c480beb3f06f555e8863833a80b8
ZUHOWKS <jojo.vilard38@gmail.com>
2024-04-24 23:44:05 +0200

    Add page de login + implémentation de vue-router + stockage du token pour la gestion d'authentification

---
+++ src/views/Login.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+  import {ref} from "vue";
+  import axios from "axios";
+  import {useRouter} from "vue-router";
+
+  const router = useRouter();
+
+  if (localStorage.getItem("PTToken")) router.push('/app');
+
+  const email = ref('');
+  const password = ref('');
+
+  function loginForm() {
+
+    const formData = new FormData();
+    formData.append('email', email.value);
+    formData.append('password', password.value);
+
+    axios.post('http://localhost:3333/api/v1/login', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then((response) => {
+
+      localStorage.setItem("PTToken", response.data.token);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
+
+      router.push('/app');
+
+    }).catch(error => {
+      //TODO: Gestion d'erreur
+    });
+
+  }
+</script>
+
+<template>
+  <form @submit.prevent="loginForm">
+    <div>
+      <label for="email">Email :</label>
+      <input type="email" v-model="email" id="email" name="email" required>
+    </div>
+    <div>
+      <label for="password">Mot de passe :</label>
+      <input type="password" v-model="password" id="password" name="password" required>
+    </div>
+    <button type="submit">Se connecter</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
