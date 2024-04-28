14483f267c9b3c0d1d5d65b4bdf58be488bc62c5
ZUHOWKS <jojo.vilard38@gmail.com>
2024-04-28 15:18:33 +0200

    Ajout d'Axios pour les requêtes API + intercepteur authentification avec token

--- src/services.ts
+++ src/services.ts
@@ -1,6 +1,28 @@
 import axios from "axios";
+import AccountServices from "@/services/account_services";
+import {useRouter} from "vue-router";
 
 
-export const Axios = axios.create({
+const Axios = axios.create({
     baseURL: "http://localhost:3333"
 });
+
+Axios.interceptors.request.use(request => {
+    let token = AccountServices.getToken();
+
+    if (token) {
+        request.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return request;
+})
+
+Axios.interceptors.response.use(response => {
+    return response;
+}, error => {
+    if (error.response && error.response.status == 401) {
+        AccountServices.logout(useRouter());
+    }
+})
+
+export {Axios}
