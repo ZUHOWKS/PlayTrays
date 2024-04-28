d6c3b7daf27b150db965d8b18db8b92a439f3919
ZUHOWKS <jojo.vilard38@gmail.com>
2024-04-28 18:39:48 +0200

    Création des composants de la vue du Menu

---
+++ src/components/menu/MainMenu.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!--
+      Conteneur pour la page principale du menu:
+      Fonctionnalité principale => Sélection du Mode de jeu et
+      lancement du matchmaking
+   -->
+</template>
+
+<style scoped>
+
+</style>
