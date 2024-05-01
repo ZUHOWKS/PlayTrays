a02a07f923efcba5924a8eca566416edbee6fe6d
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-01 14:39:30 +0200

    Implémentation du bouton logout

--- src/components/menu/TopBarMenu.vue
+++ src/components/menu/TopBarMenu.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 
+import AccountServices from "@/services/account_services";
+import {useRouter} from "vue-router";
+const router = useRouter();
+
 </script>
 
 <template>
-
+  <button class="logout" @click="AccountServices.logout(router)">
+    Logout
+  </button>
 </template>
 
 <style scoped>
