c3dd279252ae0dbe1ecb2a0eb61cd0482b3029bf
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-02 03:41:56 +0200

    Add middleware pour authentifier un serveur jeu

---
+++ app/middleware/auth_server_middleware.ts
@@ -0,0 +1,31 @@
+import type {HttpContext} from "@adonisjs/core/http";
+import type {NextFn} from "@adonisjs/core/types/http";
+import env from "#start/env";
+import db from "@adonisjs/lucid/services/db";
+
+export default class AuthServerMiddleware {
+
+  /**
+   * Vérifier les crédences de l'authentification d'un serveur jeu.
+   *
+   * @param request
+   * @param response
+   * @param next
+   */
+  async handle({request, response}: HttpContext, next: NextFn) {
+    const { identifier, token }: {identifier: string, token: string} = request.only(['identifier', 'token'])
+
+    if (identifier && token) {
+      const servers = await db.from('pt_servers').count('* as total');
+
+      for (let i: number = 1; i <= servers[0].total; i++) {
+
+        if (env.get("GS_" + i + "_IDENTIFIER") === identifier && env.get("GS_" + i + "_TOKEN") === token) {
+          return next()
+        }
+      }
+    }
+
+    return response.abort("Error Middleware!")
+  }
+}
