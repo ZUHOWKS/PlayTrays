fa23db5ce58c342edd5bc2ae35536abb10b59be0
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-02 03:46:55 +0200

    Add routes '/api/v1/server' pour l'API server :  → ajout de la route 'manifest'

--- start/routes.ts
+++ start/routes.ts
@@ -11,19 +11,26 @@ import router from '@adonisjs/core/services/router'
 import AuthController from '#controllers/auth_controller'
 import { middleware } from '#start/kernel'
 import UserController from "#controllers/user_controller";
-
+import ServerController from "#controllers/server_controller";
 
 router.group(() => {
+
+  /**
+   * REST API V1
+   * (/api/v1)
+   *
+   */
   router.group(() => {
 
     /*
-        Register & Login Routes
+        Register & Login routes
      */
     router.post('register', [AuthController, 'register'])
     router.post('login', [AuthController, 'login'])
 
     /*
         Logged routes
+        (/api/v1/user)
      */
     router.group(() => {
 
@@ -31,10 +38,18 @@ router.group(() => {
         router.get('info', [UserController, 'info'])
       }).prefix('user')
 
-
     }).use(middleware.auth({
       guards: ['api']
     }))
+
+    /*
+        Server routes
+        (/api/v1/server)
+     */
+    router.group(() => {
+      router.post('manifest', [ServerController, 'manifest'])
+    }).prefix('server')
+      .use(middleware.authServer())
 
   }).prefix('v1')
 }).prefix('api')
