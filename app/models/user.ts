808d08732456656cc93d7e819e646d85f115f1de
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-03 16:58:55 +0200

    Add some methods:  → Méthode pour obtenir la liste des utilisateurs dans un groupe ou le groupe courant de l'utilisateur
     → Méthode pour savoir si l'utilisateur est leader (s'il n'est pas membre d'un groupe alors, il est forcément leader)

--- app/models/user.ts
+++ app/models/user.ts
@@ -4,6 +4,8 @@ import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import db from "@adonisjs/lucid/services/db";
+import Group from "#models/group";
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -40,4 +42,49 @@ export default class User extends compose(BaseModel, AuthFinder) {
     type: 'auth_token',
     tokenSecretLength: 40,
   })
+
+  /**
+   * Obtenir la liste des joueurs groupés
+   * @param groupID
+   * @deprecated
+   */
+  static async getGroupedUsers(groupID: number) {
+    return User
+      .query()
+      .join('user_groups', (query) => {
+        query
+          .on('users.id', '=', 'user_groups.user_id')
+      }).join('groups', 'groups.leader_id', 'users.id')
+      .groupBy('groups.id')
+      .where('user_groups.group_id', groupID)
+      .where('groups.id', groupID)
+  }
+
+  /**
+   * Obtenir la list le groupe dans lequel est l'utilisateur.
+   * Null si l'utilisateur n'est pas groupé.
+   */
+  public async getGroup(): Promise<Group | null> {
+      return Group.find((await db.from('groups')
+          .select('groups.id as group')
+          .join('user_groups', (query) => {
+            query
+              .on('groups.id', '=', 'user_groups.group_id')
+          })
+          .where('groups.leader_id', this.id)
+          .orWhere('user_groups.user_id', this.id)
+          .first()
+      ).group)
+  }
+
+  /**
+   * Permet de savoir si le joueur est leader d'un groupe.
+   */
+  public async isLeader() {
+    return (await db.from('user_groups')
+        .select('user_id')
+        .where('user_id', '!=', this.id)
+        .first()
+    ) == null
+  }
 }
