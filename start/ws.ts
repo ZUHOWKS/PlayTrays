83e7640fecee02d0c9af364cc4e4a5f3d50b79ae
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-03 17:20:12 +0200

    Add reset des groupes en DB

--- start/ws.ts
+++ start/ws.ts
@@ -2,15 +2,20 @@ import app from '@adonisjs/core/services/app'
 import AdonisWS from "#services/adonis_ws";
 import PTServer from "#models/pt_server";
 import Lobby from "#models/lobby";
+import Group from "#models/group";
 
 app.ready(() => {
-
+  // reset des groupes (en cascade sur la table 'user_groups')
+  Group.all()
+    .then((groups: Group[]) => groups.forEach((group: Group) => group.delete()))
+  // reset des lobbies (en cascade sur la table 'user_lobbies')
   Lobby.all()
-    .then((lobbys) => lobbys.forEach((lobby) => lobby.delete()))
+    .then((lobbys: Lobby[]) => lobbys.forEach((lobby: Lobby) => lobby.delete()))
     .then(() => {
       AdonisWS.boot()
       AdonisWS.initSocketEvents()
 
+      // set les serveurs jeu par défaut en offline + tentative de connection
       PTServer.all().then((servers) => {
         servers.forEach(async (server) => {
           await server.setOffline()
@@ -18,7 +23,4 @@ app.ready(() => {
         })
       })
     })
-
-
-
 })
