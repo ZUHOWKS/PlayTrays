b012f1304455e68421681c6ca7aa87c5c47645ab
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-04 02:26:54 +0200

    Fix migration commands erreur

--- start/ws.ts
+++ start/ws.ts
@@ -2,19 +2,38 @@ import app from '@adonisjs/core/services/app'
 import AdonisWS from "#services/adonis_ws";
 import PTServer from "#models/pt_server";
 import Lobby from "#models/lobby";
-import Group from "#models/group";
+import db from "@adonisjs/lucid/services/db";
 
-app.ready(() => {
+app.ready(async () => {
+
+
+  /*
   // reset des groupes (en cascade sur la table 'user_groups')
-  Group.all()
-    .then((groups: Group[]) => groups.forEach((group: Group) => group.delete()))
-  // reset des lobbies (en cascade sur la table 'user_lobbies')
-  Lobby.all()
-    .then((lobbys: Lobby[]) => lobbys.forEach((lobby: Lobby) => lobby.delete()))
-    .then(() => {
-      AdonisWS.boot()
-      AdonisWS.initSocketEvents()
+  Group.query()
+    .delete()
 
+   */
+
+
+  // reset des lobbies (en cascade sur la table 'user_lobbies')
+  Lobby.query()
+    .delete()
+
+  AdonisWS.boot()
+  AdonisWS.initSocketEvents()
+
+
+  try {
+
+    const result = await db.rawQuery(`
+      SELECT EXISTS (
+        SELECT 1
+        FROM   information_schema.tables
+        WHERE  table_name = 'pt_servers'
+      ) AS table_exists;
+    `)
+
+    if (result[0][0].table_exists > 0) {
       // set les serveurs jeu par défaut en offline + tentative de connection
       PTServer.all().then((servers) => {
         servers.forEach(async (server) => {
@@ -22,5 +41,11 @@ app.ready(() => {
           server.initConnection()
         })
       })
-    })
+    }
+
+
+  } catch (e) {
+    console.error(e)
+  }
+
 })
