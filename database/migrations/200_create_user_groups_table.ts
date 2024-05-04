c7efb7a69b4aa96293ffba1480a31d13c9700c71
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-04 02:17:55 +0200

    Edit table `user_groups` >> ajout de la colonne `invite_accepted` pour savoir si un membre a rejoint le groupe

--- database/migrations/200_create_user_groups_table.ts
+++ database/migrations/200_create_user_groups_table.ts
@@ -17,6 +17,9 @@ export default class extends BaseSchema {
         .references('id')
         .inTable('users')
         .onDelete('CASCADE')
+      table.boolean('invite_accepted')
+        .notNullable()
+        .defaultTo(false)
     })
   }
 
