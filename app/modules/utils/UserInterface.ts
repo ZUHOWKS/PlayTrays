8507186975f4baf67f1ceeffa990925cc3a32642
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-07 15:45:06 +0200

    Add fonctionnalité demande d'ami et liste d'ami

--- app/modules/utils/UserInterface.ts
+++ app/modules/utils/UserInterface.ts
@@ -1,8 +1,14 @@
-export default interface UserInterface {
+export interface UserInterface {
     id: number;
     username: string;
     points: number;
     updatedAt: string;
     createdAt: string;
+}
 
+export interface FriendInterface {
+  id: number
+  username: string
+  online: boolean
+  status: string
 }
