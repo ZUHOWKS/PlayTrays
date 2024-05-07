20ddbdc91021d56bf4cae89c18ae341d75f3b829
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-07 15:46:19 +0200

    Add contrôleur pour lister les amis et les demandes d'ami

--- app/controllers/user_controller.ts
+++ app/controllers/user_controller.ts
@@ -1,6 +1,7 @@
 import type {HttpContext} from '@adonisjs/core/http'
 import User from "#models/user";
-import UserInterface from "../modules/utils/UserInterface.js";
+import {FriendInterface, UserInterface} from "../modules/utils/UserInterface.js";
+
 
 export default class UserController {
   async info({ auth }: HttpContext) {
@@ -17,5 +18,97 @@ export default class UserController {
     }
 
     return;
+  }
+
+  async inviteFriend({ auth, request, response }: HttpContext) {
+    if (auth.isAuthenticated) {
+      const { email } = request.only(['email'])
+      if (email) {
+        const userToInvite: User | null = await User.findBy('email', email)
+
+        if (userToInvite) {
+          const u: User | null = auth.getUserOrFail();
+
+          if (!(await u.isFriend(userToInvite)) && userToInvite.id != u.id) {
+            await u.friendInvitation(userToInvite)
+          }
+
+          return response.ok('Friend invitation send to ' + userToInvite.username)
+        }
+      }
+    }
+
+    return response.abort('Unauthorized!')
+  }
+
+  async acceptFriend({ auth, request, response }: HttpContext) {
+    const { id } = request.only(['id'])
+
+    if (id) {
+      const userToAccept: User | null = await User.find(id)
+
+      if (userToAccept) {
+        await auth.getUserOrFail().friendAccept(userToAccept)
+        return response.ok('Invite accepted !')
+      }
+    }
+
+    return response.abort('Unauthorized!')
+  }
+
+  async friends({ auth }: HttpContext){
+    if (auth.isAuthenticated) {
+      const user: User = auth.getUserOrFail()
+      const friendList: FriendInterface[] = [];
+      const _users: User[] = await user.friends()
+
+      _users.forEach((_u: User) => {
+        friendList.push({
+          id: _u.id,
+          username: _u.username,
+          online: false,
+          status: "Offline"
+        } as FriendInterface)
+      })
+      return friendList
+    }
+  }
+
+  async friendInvite({ auth }: HttpContext){
+    if (auth.isAuthenticated) {
+      const user: User = auth.getUserOrFail()
+      const friendList: FriendInterface[] = [];
+      const _users: User[] = await user.getFriendInvite()
+
+      _users.forEach((_u: User) => {
+        friendList.push({
+          id: _u.id,
+          username: _u.username,
+          online: false,
+          status: "Offline"
+        } as FriendInterface)
+      })
+
+      return friendList
+    }
+  }
+
+  async ownFriendInvite({ auth }: HttpContext){
+    if (auth.isAuthenticated) {
+      const user: User = auth.getUserOrFail()
+      const friendList: FriendInterface[] = [];
+      const _users: User[] = await user.getOwnFriendInvite()
+
+      _users.forEach((_u: User) => {
+        friendList.push({
+          id: _u.id,
+          username: _u.username,
+          online: false,
+          status: "Offline"
+        } as FriendInterface)
+      })
+
+      return friendList
+    }
   }
 }
