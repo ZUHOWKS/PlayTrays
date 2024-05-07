66b3f33e2f0a9ab8b4a94979c4c8ff92d2b5d677
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-07 16:09:39 +0200

    Add bar de navigation horizontal au top pour accéder à aux différentes pages et widget

--- src/components/menu/TopBarMenu.vue
+++ src/components/menu/TopBarMenu.vue
@@ -1,17 +1,53 @@
 <script setup lang="ts">
 
-import AccountServices from "@/services/account_services";
 import {useRouter} from "vue-router";
+
+const props = defineProps(['menuInfo', 'showSocialWidget']);
 const router = useRouter();
 
 </script>
 
 <template>
-  <button class="logout" @click="AccountServices.logout(router)">
-    Logout
-  </button>
+
+  <div class="top-bar-content">
+    <div class></div>
+    <div class="center-nav"></div>
+    <div class="left-content">
+      <svg @click="showSocialWidget" xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M402 168c-2.93 40.67-33.1 72-66 72s-63.12-31.32-66-72c-3-42.31 26.37-72 66-72s69 30.46 66 72z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="40"/><path d="M336 304c-65.17 0-127.84 32.37-143.54 95.41-2.08 8.34 3.15 16.59 11.72 16.59h263.65c8.57 0 13.77-8.25 11.72-16.59C463.85 335.36 401.18 304 336 304z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="35"/><path d="M200 185.94c-2.34 32.48-26.72 58.06-53 58.06s-50.7-25.57-53-58.06C91.61 152.15 115.34 128 147 128s55.39 24.77 53 57.94z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M206 306c-18.05-8.27-37.93-11.45-59-11.45-52 0-102.1 25.85-114.65 76.2-1.65 6.66 2.53 13.25 9.37 13.25H154" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32"/></svg>
+    </div>
+  </div>
+
+
 </template>
 
 <style scoped>
 
+  .top-bar-content {
+    top: 1.5vh;
+    height: 10vh;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    justify-items: center;
+    position: absolute;
+  }
+
+  .center-nav {
+    width: 65vw;
+  }
+
+  .left-content {
+    width: 25vw;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    padding: 0.5% 1.5%;
+  }
+
+  .left-content>svg {
+    height: 7vh;
+    cursor: pointer;
+  }
 </style>
