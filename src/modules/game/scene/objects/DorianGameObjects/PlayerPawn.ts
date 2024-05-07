05abca0e21faffd55914f894173228c6baabbaf4
Majurax <legeekmajurax@gmail.com>
2024-05-07 23:42:18 +0200

    Add: - Commentaires

--- src/modules/game/scene/objects/DorianGameObjects/PlayerPawn.ts
+++ src/modules/game/scene/objects/DorianGameObjects/PlayerPawn.ts
@@ -11,11 +11,33 @@ export class playerPawn extends PTObject implements AnimationQueueInterface {
     constructor(name: string, object3D: Object3D) {
         super(name, object3D);
         this.animationQueue = new AnimationQueue();
+
+        /*
+        Voici comment sont comptées les cases du plateau:
+
+          10 B  B  B  B  B  B  B  B  B  B  B
+          9  B                             B
+          8  B                             B
+          7  B                             B
+          6  B                             B
+          5  B                             B
+          4  B                             B
+          3  B                             B
+          2  B                             B
+          1  B                             B
+          0  B  B  B  B  B  B  B  B  B  B  B
+             0  1  2  3  4  5  6  7  8  9  10
+
+         */
         this.case = {casePawnX : 0 ,casePawnY : 0}
     }
 
+    // Permet d'avancer jusque la case choisie
     moveTo(caseX : number, caseY : number): void {
+        //Gere la cas ou la case demandée est la même que celle ou est le pion
         if (this.case.casePawnX == caseX && this.case.casePawnY == caseY) {this.moveCase();}
+
+        //Avance d'une case jusqu'à être à celle demandée
         while (this.case.casePawnX != caseX || this.case.casePawnY != caseY) {
             this.moveCase();
         }
@@ -24,27 +46,35 @@ export class playerPawn extends PTObject implements AnimationQueueInterface {
     rotate(x: number, y: number, z: number): void {
     }
 
-    select(): void {
-        this.moveTo(0, 0);
+    //Juste pour les tests (modifié plus tard)
+    select(x: number, y: number): void {
+        this.moveTo(x, y);
     }
 
     unselect(): void {
     }
 
+    //Avance jusque la case suivante de l'objet
     moveCase() : void {
-
+            //Initialisation de constantes utiles
+            const GRANDSAUT : number = 2.325;
+            const PETITSAUT : number = 1.795;
             const vectDepart: Vector3 = this.positionTest.clone();
             const position: Vector3 = this.object3D.position;
             const dir = ((this.case.casePawnX > 0 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY > 0)) ? 1 : -1;
+
+            //Definis la direction et la longueur du saut en fonction de la position sur le plateau
             const saut = ((this.case.casePawnX == 0 && this.case.casePawnY == 0) || (this.case.casePawnX == 9 && this.case.casePawnY == 0) || (this.case.casePawnX == 10 && this.case.casePawnY == 0) || (this.case.casePawnX == 10 && this.case.casePawnY == 9) || (this.case.casePawnX == 10 && this.case.casePawnY == 10) || (this.case.casePawnX == 1 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY == 1)
-            ) ? 2.325 * dir : 1.795 * dir;
+            ) ? GRANDSAUT * dir : PETITSAUT * dir;
+
+            //Definis les valeurs d'arrivée en X et Z
             const tempX: number = ((this.case.casePawnX > 0 && this.case.casePawnY == 10) || (this.case.casePawnX < 10 && this.case.casePawnY == 0)) ? vectDepart.x + saut : vectDepart.x;
             const tempZ: number = ((this.case.casePawnX == 10 && this.case.casePawnY < 10) || (this.case.casePawnX == 0 && this.case.casePawnY > 0)) ? vectDepart.z + saut : vectDepart.z;
-
 
             const vectArrivee: Vector3 = new Vector3(tempX, 0, tempZ);
             const movVec: Vector3 = new Vector3((vectArrivee.x - vectDepart.x), 0, vectArrivee.z - vectDepart.z);
 
+            //Modifie la position du pion
             if (this.case.casePawnX < 10 && this.case.casePawnY == 0) {this.case.casePawnX += 1;}
             else if (this.case.casePawnX == 10 && this.case.casePawnY < 10) {this.case.casePawnY += 1;}
             else if (this.case.casePawnX > 0 && this.case.casePawnY == 10) {this.case.casePawnX -= 1;}
