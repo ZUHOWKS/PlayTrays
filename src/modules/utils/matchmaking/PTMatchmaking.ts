bcead2fcff09589d1e31c4c99437ffb771237a6f
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-07 16:05:26 +0200

    Réponse définie à une valeur par défaut (châine de caractère vide)

--- src/modules/utils/matchmaking/PTMatchmaking.ts
+++ src/modules/utils/matchmaking/PTMatchmaking.ts
@@ -1,7 +1,7 @@
 import type {MatchmakingResponse} from "@/modules/utils/matchmaking/MatchmakingResponse";
 
 export default class PTMatchmaking {
-    response: MatchmakingResponse | undefined;
+    response: MatchmakingResponse = {message: ''};
     canStart: boolean = false;
     isInQueue: boolean = false;
 }
