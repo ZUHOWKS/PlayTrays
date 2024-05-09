8174ca3ac7666387da96ff0cd2df0e4cc38900a3
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-09 11:19:37 +0200

    Possibilité d'intérrompre et relancer le matchmaking en rappuyant sur le 'play-button'

--- src/components/menu/MainMenu.vue
+++ src/components/menu/MainMenu.vue
@@ -21,7 +21,7 @@ const modeChoose: Ref<string> = ref('Choose')
     </div>
   </div>
 
-  <button class="play-button" @click="props.startMatchmaking(modeChoose.toLowerCase())" :disabled="modeChoose.toLowerCase() ==='choose' || !props.menuInfo.matchmaking.canStart || props.menuInfo.matchmaking.isInQueue">
+  <button class="play-button" @click="props.startMatchmaking(modeChoose.toLowerCase())" :disabled="modeChoose.toLowerCase() ==='choose' || !props.menuInfo.matchmaking.canStart">
     Play : {{modeChoose.length > 10 ? modeChoose.substring(0, 7) + "..." : modeChoose}}
   </button>
 
