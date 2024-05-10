6dfbc09981c68725dd898073fddc86d578087d68
Majurax <legeekmajurax@gmail.com>
2024-05-10 02:18:10 +0200

    Add: - Animation pour l'apparition et la dispartion d'une carte d'information unique pour chcune des villes (ajout des cartes chances, bataille ou autre potentiellement plus tard dans le projet)

--- src/modules/game/scene/controllers/DorianController.ts
+++ src/modules/game/scene/controllers/DorianController.ts
@@ -1,6 +1,15 @@
 import SupportController from "@/modules/game/scene/SupportController";
 import type Actuator from "@/modules/game/scene/actionners/Actuator";
-import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneGeometry, Scene} from "three";
+import {
+    BoxGeometry,
+    DoubleSide,
+    Mesh,
+    MeshBasicMaterial,
+    PerspectiveCamera,
+    PlaneGeometry,
+    Scene,
+    Vector3
+} from "three";
 import type {Ref} from "vue";
 import type {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import type {Socket} from "socket.io-client";
@@ -11,8 +20,10 @@ import {prison} from "@/modules/game/scene/objects/DorianGameObjects/Prison";
 import type PTObject from "@/modules/game/scene/objects/PTObject";
 import {playerPawn} from "@/modules/game/scene/objects/DorianGameObjects/PlayerPawn";
 import {CaseSelector} from "@/modules/game/scene/objects/DorianGameObjects/CaseSelector";
+import Pawn from "@/modules/game/scene/objects/Pawn";
 
 export default class DorianGame extends SupportController{
+
     constructor(scene: Scene, cameraRef: Ref<PerspectiveCamera>, orbitControlsRef: Ref<OrbitControls>, ws: Socket) {
         super(scene, cameraRef, orbitControlsRef, ws);
     }
@@ -20,6 +31,7 @@ export default class DorianGame extends SupportController{
     }
 
     defaultCamera(): void {
+        //this.displayCardHUD();
     }
 
     getSelectedActuator(): Actuator | null | undefined {
@@ -27,6 +39,9 @@ export default class DorianGame extends SupportController{
     }
 
     selectActuator(name: string): void {
+    }
+
+    unselectObject() {
     }
 
     // Permet d'initialiser le jeu DorianGame lors du lancement de la page
@@ -99,24 +114,74 @@ export default class DorianGame extends SupportController{
 
     unselectAll(): void {
     }
+
     selectObject(name: string) {
+
+        //Abaisse la carte d'information si l'objet cliqué est la même carte que celle deja en mémoire
+        if (this.selectedObject && this.selectedObject instanceof CaseSelector && this.selectedObject.getName() == name) {(document.getElementsByClassName("hud")[0] as HTMLElement).style.transform = "translateY(60vh)";}
+
+        //Sinon on effectue le code classique
+        else{
+
         super.selectObject(name);
 
         //Si l'objet selectionné est une caseselector, on applique des modififcations en fonction du type de la case
-        if (this.selectedObject && this.selectedObject instanceof CaseSelector){
-            const tempcase : CaseSelector = this.selectedObject as CaseSelector;
-            if (tempcase.case.caseType == "start"){}
-            else if (tempcase.case.caseType == "prison"){this.getObject("Prison")?.select();}
-            else if (tempcase.case.caseType == "bank"){}
-            else if (tempcase.case.caseType == "war"){}
-            else if (tempcase.case.caseType == "chance"){}
-            else if (tempcase.case.caseType == "bataille"){}
-            else {}
+        if (this.selectedObject && this.selectedObject instanceof CaseSelector) {
+            const tempcase: CaseSelector = this.selectedObject as CaseSelector;
+            if (tempcase.case.caseType == "start") {
+            } else if (tempcase.case.caseType == "prison") {
+                this.getObject("Prison")?.select();
+            } else if (tempcase.case.caseType == "bank") {
+            } else if (tempcase.case.caseType == "war") {
+            } else if (tempcase.case.caseType == "chance") {
+            } else if (tempcase.case.caseType == "bataille") {
+            } else {
+                //Ajoute les parametres propres aux villes dans la carte d'information
+                (document.getElementsByClassName("name")[0] as HTMLElement).innerText = ""+tempcase.case.cityName;
+                (document.getElementsByClassName("title")[0] as HTMLElement).style.backgroundColor = tempcase.case.colorCase;
+                (document.getElementsByClassName("price-default")[0] as HTMLElement).innerText = "£"+tempcase.case.m0;
+                (document.getElementsByClassName("price-1")[0] as HTMLElement).innerText = "£"+tempcase.case.m1;
+                (document.getElementsByClassName("price-2")[0] as HTMLElement).innerText = "£"+tempcase.case.m2;
+                (document.getElementsByClassName("price-3")[0] as HTMLElement).innerText = "£"+tempcase.case.m3;
+                (document.getElementsByClassName("price-4")[0] as HTMLElement).innerText = "£"+tempcase.case.m4;
+                (document.getElementsByClassName("price-5")[0] as HTMLElement).innerText = "£"+tempcase.case.m5;
+                (document.getElementsByClassName("price-hotel")[0] as HTMLElement).innerText = "£"+tempcase.case.maison + "\nplus 4 maisons";
+                (document.getElementsByClassName("price-maison")[0] as HTMLElement).innerText = "£"+tempcase.case.maison;
+
+
+
+                (document.getElementsByClassName("hud")[0] as HTMLElement).style.transform = "translateY(0vh)";
+            }
 
             //Juste pour les tests (avance jusque la case et affiche des choses importantes)
-            this.getObject("PlayerPawn3")?.select(tempcase.case.casePawnX, tempcase.case.casePawnY);
-            console.log("Carte choisie: " + tempcase.case.caseType + ".\nVoici les coordonnées: " + tempcase.case.casePawnX + ", "+ tempcase.case.casePawnY + "\nVoici l'addition des cos: " + tempcase.case.casePawnX + tempcase.case.casePawnY);
-        }
+            //this.getObject("PlayerPawn3")?.select(tempcase.case.casePawnX, tempcase.case.casePawnY);
+            //this.displayCardHUD();
 
+            //Tests pour verifier les coordonnées
+            console.log("Carte choisie: " + tempcase.case.caseType + ".\nVoici les coordonnées: " + tempcase.case.casePawnX + ", " + tempcase.case.casePawnY + "\nVoici l'addition des cos: " + tempcase.case.casePawnX + tempcase.case.casePawnY);
+
+        }
     }
+        }
+    // Inutile car trop d'inconvénients
+    displayCardHUD(): void {
+        const cam = this.getCamera();
+
+        const direction = new Vector3();
+        cam.getWorldDirection(direction);
+
+        const geometry = new PlaneGeometry(1.5, 2);
+        const material = new MeshBasicMaterial({ color: 0x000000});
+
+        const cubeTest : Mesh<PlaneGeometry, MeshBasicMaterial> = new Mesh(geometry, material);
+
+        cubeTest.position.copy(cam.position).addScaledVector(direction, 5);
+        cubeTest.lookAt(cam.position);
+
+        this.scene.add(cubeTest);
+
+        console.log("position: \nx: " + Math.round(cam.position.x) + "\ny: " + Math.round(cam.position.y) + "\nz: " + Math.round(cam.position.z) + "\n\nrotation: \nx: " + (cam.rotation.x).toFixed(2) + "\ny: " + Math.round(cam.rotation.y) + "\nz: " + Math.round(cam.rotation.z));
+    }
+
+
 }
