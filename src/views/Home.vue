67a61b097cd9ec4100af06c581c5d56788df3172
Mathvar <mathvar2004@gmail.com>
2024-05-14 14:04:55 +0200

    Ajout GAMES

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,18 @@ import SNavbar from "@/components/home/SNavbar.vue";
 import Welcome from "@/components/home/Welcome.vue";
 import Footer from "@/components/home/Footer.vue";
 import About from "@/components/home/About.vue";
+import Games from "@/components/home/Games.vue";
 </script>
 
 <template>
   <SNavbar></SNavbar>
   <main>
     <Welcome></Welcome>
-  </main>
-  <main>
+
     <About></About>
-  </main>
-  <main>
+    <Games/>
     <Footer></Footer>
+
   </main>
 </template>
 
