b5c78131d4db8876b31555e979473fd757221a6c
OthGart <othmanegartani@gmail.com>
2024-05-14 14:18:11 +0200

    Modification du design de la page login.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-  import {ref} from "vue";
+import {onMounted, type Ref, ref} from "vue";
   import {useRouter} from "vue-router";
   import AccountServices from "@/services/account_services";
 
   const router = useRouter();
 
   if (AccountServices.isLogged()) router.push('/app');
+
+  const isLoginMode: Ref<boolean> = ref(true);
+  const signUpMode: Ref<HTMLElement | null> = ref(null)
+  const loginMode: Ref<HTMLElement | null> = ref(null)
+  const signFiller: Ref<HTMLElement | null> = ref(null)
 
   const email = ref('');
   const password = ref('');
@@ -25,101 +30,165 @@
     });
 
   }
+
+  function switchSignMode() {
+    if (signUpMode.value && loginMode.value && signFiller.value) {
+      isLoginMode.value = !isLoginMode.value
+      if (!isLoginMode.value) {
+        signUpMode.value.style.transform = "translateX(0)"
+
+      } else {
+        signUpMode.value.style.transform = "translateX(-200%)"
+      }
+    }
+  }
 </script>
 
 <template>
   <header @click="() => router.push('/')">
-    <img src="@/assets/image/logoPT(1).png" alt="PlayTrays Logo" class="logo">
-    <h1>PlayTrays</h1>
+    <div class="header-content">
+      <img src="@/assets/image/logoPT(1).png" alt="PlayTrays Logo" class="logo">
+      <h1>PlayTrays</h1>
+    </div>
   </header>
   <main>
-    <form @submit.prevent="loginForm" class="test">
-      <div>
-        <label for="email">Email :</label>
-        <input type="email" v-model="email" id="email" name="email" required>
+    <div class="row forms">
+      <form @submit.prevent="loginForm" class="test">
+        <h2>Login</h2>
+        <div class="inputs">
+          <input type="email" v-model="email" id="email" name="email" required placeholder="Email">
+          <input type="password" v-model="password" id="password" name="password" required placeholder="Password">
+        </div>
+        <button type="submit" class="login-button">Login</button>
+      </form>
+      <form @submit.prevent="loginForm" class="test">
+        <div>
+          <input type="email" v-model="email" id="email" name="email" required>
+        </div>
+        <div>
+          <input type="password" v-model="password" id="password" name="password" required>
+        </div>
+        <button type="submit" class="login-button">Login</button>
+      </form>
+    </div>
+    <div class="sign-mode-filler" ref="signFiller">
+      <div class="login-mode sign-mode-content" ref="loginMode">
+        <h2>Welcome Back !</h2>
+        <button @click="switchSignMode" class="switch-button">You are not register ?</button>
       </div>
-      <div>
-        <label for="password">Password :</label>
-        <input type="password" v-model="password" id="password" name="password" required>
+      <div class="sign-up-mode sign-mode-content" ref="signUpMode">
+        <h2>Welcome !</h2>
+        <button @click="switchSignMode" class="switch-button">You have an account ?</button>
       </div>
-      <button type="submit" class="r_button">Login</button>
-    </form>
+    </div>
   </main>
 </template>
 
 <style scoped>
 header {
-  width: 4vw;
+  height: 10vh;
+  position: relative;
+  margin : 1% 5%;
   display : flex;
-  flex-direction : row;
-  cursor : pointer;
+  justify-content: center;
 }
 
 h1 {
-  width : 1vw;
-  height : 1vh;
   font-size : 2.62vw;
 }
 
 .logo {
-  margin : 5%;
-  width: 95%;
-  height: 5%;
-  transition: transform 0.6s ease-in-out; 
+
+  height: 5vh;
+  transition: transform 0.6s ease-in-out;
 }
 
-.logo:hover {
-    transform: rotate(360deg); 
+.header-content {
+  cursor : pointer;
+  display : flex;
+  flex-direction : row;
+  align-items: center;
+  justify-content: center;
+  width: max-content;
+  margin: 0 50%;
+}
+
+.header-content:hover>.logo {
+    transform: rotate(360deg);
 }
 
 main {
+  width: 100vw;
+  height: 90vh;
   display: flex;
-  justify-content : center;
-  border-top: 2px solid black;
-  /* background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLK28gM_Wd78MjaifMEzGLphnCyKnXgpjfHA&s);
-  background-position: center;
-  background-size: cover; */
-  background : linear-gradient(#edf065, #d077e2c7);
-  height : 92.5vh;
-  width : 100vw;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 10vh;
+  left: 0;
 }
 
-.test {
+.forms {
+  border-radius: 20px;
+  align-items: center;
+  width: 60%;
+  height: 70%;
+  box-shadow: 0px 8px 20px rgba(0,0,0, 0.15);
+}
+
+.forms>form {
+  width: 100%;
+  margin: 2% 4%;
   display: flex;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
-  align-content: center;
-  margin: 10%;
-  padding: 7%;
-  border: 1px solid #000;
-  border-radius: 3vw;
-  background-color: #f3f7d5;
 }
 
-.test>div {
-  display : grid;
-  grid-template-columns : 55% 85%;
-  font-size: 1.5vw;
-  font-style: italic;
-  font-weight: 600;
+form>.inputs {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
   justify-content: center;
-  margin-bottom : 10%;
+  margin: 5% 0;
 }
 
-input {
-  font-size : 0.7em;
-  color:#5e5e56;
+.inputs>input {
+  width: 95%;
+  height: 4vh;
+  font-size: 2vh;
+  margin: 2.5% 2.5%;
+  padding: 0.25% 1%;
+  background: rgba(var(--link-color), 0.25);
+  border: none;
 }
 
-.r_button {
-  width : 12vw;
-  height : 5vh;
-  font-size : 1.6vw;
-  font-family: "Poetsen One", sans-serif;
-  background-color : rgba(255, 255, 255, 0.6);
-  box-shadow : 0.7vw 0.7vh 1.5em rgba(0, 0, 0, 0.5);
-  margin-top: 7vh;
-  align-self: center;
+.sign-mode-filler {
+  position: absolute;
+  background: linear-gradient(120deg, rgb(var(--primary-color)) -175%, rgb(var(--secondary-color)) 100%);
+  width: 30%;
+  left: 50%;
+  height: 70%;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+}
+
+.sign-mode-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+}
+
+.sign-up-mode {
+  transform: translateX(-200%);
+  transition: transform 1s 0.5s ease-in-out;
 }
 
 </style>
