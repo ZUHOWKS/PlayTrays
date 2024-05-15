0736453370d78b590bfb249357df35187a22649c
Majurax <legeekmajurax@gmail.com>
2024-05-15 02:25:59 +0200

    Add: - Affichage des informations des joueurs lorsque leur pion est cliqué
    Update
    - Variable permettant de désactiver l'animation du pion dans moveCase pour permettre au serveur de setup la position du pion sur le coté client instantanement

--- src/modules/game/scene/objects/DorianGameObjects/PlayerPawn.ts
+++ src/modules/game/scene/objects/DorianGameObjects/PlayerPawn.ts
@@ -5,12 +5,12 @@ import {AnimationQueue, type AnimationQueueInterface} from "@/modules/utils/Anim
 
 export class playerPawn extends PTObject implements AnimationQueueInterface {
     animationQueue: AnimationQueue;
-    case : {casePawnX : number ,casePawnY : number, casseNb: number}
+    case : {casePawnX : number ,casePawnY : number}
     positionTest : Vector3 = new Vector3(0, 0, 0);
     user: string;
 
 
-    constructor(name: string, object3D: Object3D, user: string) {
+    constructor(name: string, object3D: Object3D, user : string) {
         super(name, object3D);
         this.animationQueue = new AnimationQueue();
         this.user = user;
@@ -29,15 +29,14 @@ export class playerPawn extends PTObject implements AnimationQueueInterface {
           2  B                             B
           1  B                             B
           0  B  B  B  B  B  B  B  B  B  B  B
-             10 9  8  7  6  5  4  3  2  1  0
+             0  1  2  3  4  5  6  7  8  9  10
 
          */
-        this.case = {casePawnX : 0 ,casePawnY : 0, casseNb: 0}
+        this.case = {casePawnX : 0 ,casePawnY : 0}
     }
 
     // Permet d'avancer jusque la case choisie
     moveTo(caseX : number, caseY : number): void {
-
         //Gere la cas ou la case demandée est la même que celle ou est le pion
         if (this.case.casePawnX == caseX && this.case.casePawnY == caseY) {this.moveCase();}
 
@@ -52,62 +51,60 @@ export class playerPawn extends PTObject implements AnimationQueueInterface {
 
     //Juste pour les tests (modifié plus tard)
     select(): void {
+        const userElem = (document.querySelector('#other-user') as HTMLElement);
+        userElem.style.transform = 'translateX(0)';
+        (userElem.querySelector('.username') as HTMLElement).innerHTML = this.user;
     }
 
     unselect(): void {
+        const userElem = (document.querySelector('#other-user') as HTMLElement);
+        userElem.style.transform = 'translateX(150%)';
     }
 
     //Avance jusque la case suivante de l'objet
-    moveCase(anim: boolean = true) : void {
-            //Initialisation de constantes utiles
-            const GRANDSAUT : number = 2.325;
-            const PETITSAUT : number = 1.795;
-            const vectDepart: Vector3 = this.positionTest.clone();
-            const position: Vector3 = this.object3D.position;
-            const dir = ((this.case.casePawnX > 0 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY > 0)) ? 1 : -1;
+    moveCase() : void {
+        //Initialisation de constantes utiles
+        const GRANDSAUT : number = 2.325;
+        const PETITSAUT : number = 1.795;
+        const vectDepart: Vector3 = this.positionTest.clone();
+        const position: Vector3 = this.object3D.position;
+        const dir = ((this.case.casePawnX > 0 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY > 0)) ? 1 : -1;
 
-            //Definis la direction et la longueur du saut en fonction de la position sur le plateau
-            const saut = ((this.case.casePawnX == 0 && this.case.casePawnY == 0) || (this.case.casePawnX == 9 && this.case.casePawnY == 0) || (this.case.casePawnX == 10 && this.case.casePawnY == 0) || (this.case.casePawnX == 10 && this.case.casePawnY == 9) || (this.case.casePawnX == 10 && this.case.casePawnY == 10) || (this.case.casePawnX == 1 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY == 1)
-            ) ? GRANDSAUT * dir : PETITSAUT * dir;
+        //Definis la direction et la longueur du saut en fonction de la position sur le plateau
+        const saut = ((this.case.casePawnX == 0 && this.case.casePawnY == 0) || (this.case.casePawnX == 9 && this.case.casePawnY == 0) || (this.case.casePawnX == 10 && this.case.casePawnY == 0) || (this.case.casePawnX == 10 && this.case.casePawnY == 9) || (this.case.casePawnX == 10 && this.case.casePawnY == 10) || (this.case.casePawnX == 1 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY == 10) || (this.case.casePawnX == 0 && this.case.casePawnY == 1)
+        ) ? GRANDSAUT * dir : PETITSAUT * dir;
 
-            //Definis les valeurs d'arrivée en X et Z
-            const tempX: number = ((this.case.casePawnX > 0 && this.case.casePawnY == 10) || (this.case.casePawnX < 10 && this.case.casePawnY == 0)) ? vectDepart.x + saut : vectDepart.x;
-            const tempZ: number = ((this.case.casePawnX == 10 && this.case.casePawnY < 10) || (this.case.casePawnX == 0 && this.case.casePawnY > 0)) ? vectDepart.z + saut : vectDepart.z;
+        //Definis les valeurs d'arrivée en X et Z
+        const tempX: number = ((this.case.casePawnX > 0 && this.case.casePawnY == 10) || (this.case.casePawnX < 10 && this.case.casePawnY == 0)) ? vectDepart.x + saut : vectDepart.x;
+        const tempZ: number = ((this.case.casePawnX == 10 && this.case.casePawnY < 10) || (this.case.casePawnX == 0 && this.case.casePawnY > 0)) ? vectDepart.z + saut : vectDepart.z;
 
-            const vectArrivee: Vector3 = new Vector3(tempX, 0, tempZ);
-            const movVec: Vector3 = new Vector3((vectArrivee.x - vectDepart.x), 0, vectArrivee.z - vectDepart.z);
+        const vectArrivee: Vector3 = new Vector3(tempX, 0, tempZ);
+        const movVec: Vector3 = new Vector3((vectArrivee.x - vectDepart.x), 0, vectArrivee.z - vectDepart.z);
 
-            //Modifie la position du pion
-            if (this.case.casePawnX < 10 && this.case.casePawnY == 0) {this.case.casePawnX += 1;}
-            else if (this.case.casePawnX == 10 && this.case.casePawnY < 10) {this.case.casePawnY += 1;}
-            else if (this.case.casePawnX > 0 && this.case.casePawnY == 10) {this.case.casePawnX -= 1;}
-            else if (this.case.casePawnX == 0 && this.case.casePawnY > 0) {this.case.casePawnY -= 1;}
-            this.positionTest.x = vectArrivee.x;
-            this.positionTest.y = vectArrivee.y;
-            this.positionTest.z = vectArrivee.z;
+        //Modifie la position du pion
+        if (this.case.casePawnX < 10 && this.case.casePawnY == 0) {this.case.casePawnX += 1;}
+        else if (this.case.casePawnX == 10 && this.case.casePawnY < 10) {this.case.casePawnY += 1;}
+        else if (this.case.casePawnX > 0 && this.case.casePawnY == 10) {this.case.casePawnX -= 1;}
+        else if (this.case.casePawnX == 0 && this.case.casePawnY > 0) {this.case.casePawnY -= 1;}
+        this.positionTest.x = vectArrivee.x;
+        this.positionTest.y = vectArrivee.y;
+        this.positionTest.z = vectArrivee.z;
 
-            if (anim){
-                this.animationQueue.push({
-                    duration: 100,
-                    startCallback: (): void => {
-                    },
-                    animationCallback: (progress: number): void => {
-                        position.x = vectDepart.x + movVec.x * progress;
-                        position.y = vectDepart.y + movVec.y * progress;
-                        position.z = vectDepart.z + movVec.z * progress;
-                    },
-                    finalCallback: (): void => {
-                        position.x = vectArrivee.x;
-                        position.y = vectArrivee.y;
-                        position.z = vectArrivee.z;
-                    }
-                })
-            }
-            else {
+
+        this.animationQueue.push({
+            duration: 100,
+            startCallback: (): void => {
+            },
+            animationCallback: (progress: number): void => {
+                position.x = vectDepart.x + movVec.x * progress;
+                position.y = vectDepart.y + movVec.y * progress;
+                position.z = vectDepart.z + movVec.z * progress;
+            },
+            finalCallback: (): void => {
                 position.x = vectArrivee.x;
                 position.y = vectArrivee.y;
                 position.z = vectArrivee.z;
             }
-
-        }
+        })
+    }
 }
