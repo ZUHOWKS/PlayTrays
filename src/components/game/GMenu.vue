5c6ff50dc1a21c3db190cabefbadb355691ff895
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-16 14:51:25 +0200

    Ajout d'un menu en jeu >> permet de quitter la partie (forfait si la partie n'est pas terminé

---
+++ src/components/game/GMenu.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  const props=defineProps(['leaveParty'])
+</script>
+
+<template>
+  <div class="game-menu">
+    <button @click="leaveParty">Leave the party</button>
+  </div>
+</template>
+
+<style scoped>
+.game-menu {
+  position: fixed;
+  width: 22.5vw;
+  height: 35vw;
+  top: 15%;
+  left: 39.75vw;
+  z-index: 3;
+  background: rgb(var(--background-color));
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.15);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
