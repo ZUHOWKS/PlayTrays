a08df4d136dd955bb1aee3222ed035f86a16072d
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-16 20:50:22 +0200

    .

--- src/components/utils/SocialWidget.vue
+++ src/components/utils/SocialWidget.vue
@@ -54,9 +54,9 @@ async function getAllFriendInvitations(): Promise<void> {
       <div v-for="friend in menuInfo.friendList" class="friend-card" @click="inviteInGroup(friend.id)">
         <div class="row">
           <h3>{{ friend .username }}</h3>
-          <svg xmlns="http://www.w3.org/2000/svg" :class="[friend?.online ? 'online-indicator' : 'offline-indicator']" viewBox="0 0 512 512"><path d="M256 464c-114.69 0-208-93.31-208-208S141.31 48 256 48s208 93.31 208 208-93.31 208-208 208z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" :class="[friend.online ? 'online-indicator' : 'offline-indicator']" viewBox="0 0 512 512"><path d="M256 464c-114.69 0-208-93.31-208-208S141.31 48 256 48s208 93.31 208 208-93.31 208-208 208z"/></svg>
         </div>
-        <p>{{ friend?.status.length > 20 ? friend?.status.substring(0, 17) + "..." : friend?.status }}</p>
+        <p>{{ friend.status.length > 20 ? friend.status.substring(0, 17) + "..." : friend.status }}</p>
       </div>
     </div>
 
