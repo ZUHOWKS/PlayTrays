319de35906c841469f5130f3d6fdfd7a6a143fd4
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-17 10:28:31 +0200

    Configuration de l'host pour déploiement en développement

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ require('dotenv').config();
 
 const httpServer = createServer();
 const gServer: PTServer = new PTServer(httpServer, 10);
-httpServer.listen(25526)
+httpServer.listen(25526, '0.0.0.0')
 gServer.init();
