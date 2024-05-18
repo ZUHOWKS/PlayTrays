de399dfd41c1e4e5bff89ccd7c1fe3b6c08dd274
Majurax <legeekmajurax@gmail.com>
2024-05-19 00:13:22 +0200

    Update: - Changement de la méthode de construction

--- src/modules/game/scene/objects/DorianGameObjects/CaseSelector.ts
+++ src/modules/game/scene/objects/DorianGameObjects/CaseSelector.ts
@@ -1,31 +1,14 @@
 import PTObject from "@/modules/game/scene/objects/PTObject";
 import {BoxGeometry, type Color, Mesh, MeshBasicMaterial, type Object3D, Vector3} from "three";
-import {cardHelper, cardCityHelper} from "@/modules/game/scene/objects/DorianGameObjects/CardHelper";
-import {prison} from "@/modules/game/scene/objects/DorianGameObjects/Prison";
 
-interface ville{
-    casePawnX : number ,casePawnY : number, caseType : string, colorCase : string,  cityName : string, m0 : number, m1 : number, m2 : number, m3 : number, m4 : number, m5 : number, m6 : number, maison : number, prix : number
-}
-
-interface autre{
-    casePawnX : number ,casePawnY : number, caseType : string
-}
 
 export class CaseSelector extends PTObject{
-    case : ville | autre;
+    nbCase: number;
 
-
-    constructor(name: string, object3D: Object3D, caseX : number, caseY : number) {
+    constructor(name: string, object3D: Object3D, nbCase: number) {
         super(name, object3D);
 
-        //Permet de construire les variables en fonction de la position de la carte en utilisatn les dictionnaires CardHelper et CardColorHelper
-        let tempHelp : string = "" + caseX + caseY;
-        const caseType: string = (tempHelp in cardHelper) ? cardHelper[tempHelp as keyof typeof cardHelper] : "ville";
-        const tempcardvalues= (tempHelp in cardCityHelper) ? cardCityHelper[tempHelp as keyof typeof cardCityHelper] : null;
-        if (tempcardvalues != null && typeof tempcardvalues != "undefined" && caseType == "ville"){
-            this.case = {casePawnX : caseX, casePawnY : caseY, caseType : caseType, colorCase : tempcardvalues.color, cityName : tempcardvalues.cityName, m0 : tempcardvalues.m0, m1 : tempcardvalues.m1, m2 : tempcardvalues.m2, m3 : tempcardvalues.m3, m4 : tempcardvalues.m4, m5 : tempcardvalues.m5, m6 : tempcardvalues.m6, maison : tempcardvalues.maison, prix : tempcardvalues.prix};
-        }
-        else{this.case = {casePawnX : caseX, casePawnY : caseY, caseType : caseType};}
+        this.nbCase = nbCase
 
 
     }
@@ -39,8 +22,5 @@ export class CaseSelector extends PTObject{
     }
 
     unselect(): void {
-    }
-    afficherCase() : void {
-
     }
 }
