2cdec80a9908f99997045185d4b72edf9b023868
ZUHOWKS <jojo.vilard38@gmail.com>
2024-05-19 04:04:04 +0200

    Add scène 3D pour le main menu >> affichage des joueurs membres du groupe par des pions !

--- src/components/menu/MainMenu.vue
+++ src/components/menu/MainMenu.vue
@@ -4,40 +4,97 @@ import {ref, type Ref} from "vue";
 const props = defineProps(['menuInfo', 'startMatchmaking']);
 const modeChoose: Ref<string> = ref('Choose')
 
+const showModeSelection: Ref<boolean> = ref(false)
+
 </script>
 
 <template>
+  <slot></slot>
   <!--
       Conteneur pour la page principale du menu:
       Fonctionnalité principale => Sélection du Mode de jeu et
       lancement du matchmaking
    -->
 
-  <div class="column game-modes">
+  <div v-if="showModeSelection" class="column game-modes">
+    <h2>Mode Selection</h2>
+    <svg @click="() => {showModeSelection = !showModeSelection}" xmlns="http://www.w3.org/2000/svg" class="close-button b" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144M368 144L144 368"/></svg>
     <div class="row">
-      <div class="mode-card" @click="() => {modeChoose = 'Checkers'}">
+      <div class="mode-card" @click="() => {modeChoose = 'Checkers'; showModeSelection = false;}">
         <h3>Checkers</h3>
       </div>
     </div>
   </div>
 
-  <button class="play-button" @click="props.startMatchmaking(modeChoose.toLowerCase())" :disabled="modeChoose.toLowerCase() ==='choose' || !props.menuInfo.matchmaking.canStart">
-    Play : {{modeChoose.length > 10 ? modeChoose.substring(0, 7) + "..." : modeChoose}}
-  </button>
+  <div class="play-container column">
+    <div class="mode-select" @click="() => {showModeSelection = !showModeSelection; }">
+      <h2 class="title-selected">Checkers</h2>
+      <img src="@/assets/image/checkers_game_view.png" alt="checkers game view">
+    </div>
+    <button class="play-button" @click="props.startMatchmaking(modeChoose.toLowerCase())" :disabled="modeChoose.toLowerCase() ==='choose' || !props.menuInfo.matchmaking.canStart">
+      Play : {{modeChoose.length > 10 ? modeChoose.substring(0, 7) + "..." : modeChoose}}
+    </button>
+  </div>
+
+
 
 </template>
 
 <style scoped>
   .game-modes {
-    position: absolute;
-    top: 14vh;
-    width: 100vw;
+    position: fixed;
+    top: 12vh;
+    width: 75vw;
+    left: 12.5vw;
+    height: 80vh;
+    background-color: white;
     align-items: center;
+    border-radius: 15px;
+    z-index: 2;
+    transform: scaleY(0);
+    animation: showGameModeSelection .125s ease-in-out both;
   }
 
-  .mode-card {
-    height: 40vh;
-    width: 27.5vh;
+  @keyframes showGameModeSelection {
+    0% {
+      transform: scaleY(0);
+    }
+    100% {
+      transform: scaleY(1);
+    }
+  }
+
+  .game-modes>.close-button {
+    position: fixed;
+    height: 5vw;
+    min-height: 50px;
+    width: 5vw;
+    min-width: 50px;
+    margin: 0.75%;
+    align-self: end;
+    cursor: pointer;
+    z-index: 2;
+    color: black;
+  }
+
+  .game-modes>h2 {
+    margin: 3vh 1.5vh;
+    font-size: max(37px, 5vw);
+  }
+
+  .game-modes>.row {
+    width: 70vw;
+    height: 80%;
+    padding: 0 2.5vw;
+    overflow-y: scroll;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .game-modes>.row>.mode-card {
+    height: 42.5vh;
+    width: 30vh;
+    margin: 0 2.5%;
     background: white;
     border: solid 2px rgb(var(--primary-color));
     border-radius: 10px;
@@ -52,15 +109,48 @@ const modeChoose: Ref<string> = ref('Choose')
   }
 
   .mode-card>h3 {
-    font-size: 43px;
+    font-size: max(34px, 3vw);
+  }
+
+  .play-container {
+    position: fixed;
+    right: 2.5%;
+    bottom: 2.5vh;
+    width: 25vw;
+    min-width: 225px;
+    overflow: hidden;
+  }
+
+  .play-container>.mode-select {
+    cursor: pointer;
+    height: 15vw;
+    width: 100%;
+    min-height: 125px;
+    background: linear-gradient(0deg, rgba(255,255,255, 0.75) 0%, rgba(0,0,0,0) 20%);
+    margin: 5% 0;
+    border-radius: 15px;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: end;
+  }
+
+  .mode-select>.title-selected {
+    font-size: 3vw;
+  }
+
+  .mode-select>img {
+    z-index: -1;
+    position: absolute;
+    width: 100%;
+    border-radius: 15px;
   }
 
   .play-button {
-    position: fixed;
-    right: 2.5%;
-    bottom: 5%;
-    width: 28.5%;
-    font-size: 50px;
+
+    font-size: 2.5vw;
     font-weight: 600;
   }
 
@@ -68,4 +158,5 @@ const modeChoose: Ref<string> = ref('Choose')
     background-color: #b7b7b7;
     border-color: darkgrey;
   }
+
 </style>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 import {type Router, useRouter} from "vue-router";
-import {reactive, type Ref, ref} from "vue";
+import {computed, onMounted, reactive, type Ref, ref} from "vue";
 import AccountServices from "@/services/account_services";
 import MainMenu from "@/components/menu/MainMenu.vue";
 import TopBarMenu from "@/components/menu/TopBarMenu.vue";
@@ -16,9 +16,142 @@ import type {UserInterface, FriendInterface} from "@/modules/utils/UserInterface
 import type {GroupInterface} from "@/modules/utils/GroupInterface";
 import LoaderFiller from "@/components/utils/LoaderFiller.vue";
 import {ADONIS_URL} from "@/config/serverConfig";
+import * as THREE from "three";
+import {useWindowSize} from "@vueuse/core";
+import {ModelLoader} from "@/modules/utils/scene/ModelLoader";
+import type {Object3D} from "three";
 
 const router: Router = useRouter();
 if (!AccountServices.isLogged()) AccountServices.logout(router);
+
+/**
+ * THREE JS scene
+ */
+
+const experience: Ref<HTMLCanvasElement | null> = ref<HTMLCanvasElement | null>(null); // VueJS variable, canvas de la scène 3d
+let renderer: Ref<THREE.WebGLRenderer>; // Renderer de la caméra
+let camera: Ref<THREE.PerspectiveCamera>; // Caméra (vue du joueur)
+let scene: THREE.Scene; // Scène 3D
+let objectRegistry: Map<string, THREE.Object3D> = new Map<string, THREE.Object3D>()
+
+let {width, height} = useWindowSize(); // Permet d'obtenir la taille de l'écran actuel pour le responsive
+const aspectRatio = computed(() => width.value / height.value) // rapport largeur/hauteur
+
+/**
+ * Mettre à jour la caméra
+ */
+function updateCamera(): void {
+  camera.value.aspect = aspectRatio.value; // actualisation du rapport d'aspect de la caméra
+  camera.value.updateProjectionMatrix(); // mettre à jour la matrice de projection de la caméra
+}
+
+/**
+ * Mettre à jour le renderer
+ */
+function updateRenderer(): void {
+  renderer.value.setSize(width.value, height.value)
+  renderer.value.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+}
+
+/**
+ * Mettre à jour le rendu
+ */
+function updateRender(): void {
+  renderer.value.render(scene, camera.value)
+}
+
+/**
+ * Boucle qui rafraîchie le rendu de la scène.
+ */
+function animate(time) {
+  for (let i = 1; i <= 4; i++) {
+    const userObj: THREE.Object3D | undefined = objectRegistry.get('user'+i);
+    if (userObj) {
+      userObj.position.y = Math.cos((time + i * Math.random()) * 0.0015 + i) * 0.02
+      userObj.rotation.z = Math.sin(Math.cos((time + i * Math.random()) * 0.00075 + i) * Math.PI) * Math.PI/64 + Math.PI/8
+      userObj.rotation.y = Math.sin(Math.cos((time + i * Math.random()) * 0.00075 + i) * Math.PI) * Math.PI/64 + Math.PI/3
+    }
+  }
+
+  updateCamera(); // mise à jour de la caméra
+  updateRenderer(); // mise à jour du renderer
+  updateRender(); // mise à jour du rendu
+
+  requestAnimationFrame(animate); // permet de relancer la fonction à la frame suivante
+}
+
+async function generateCasa() {
+  const casa: THREE.Object3D = await ModelLoader.loadGLTFSceneModel(ModelLoader.GLTF_LOADER, 'casa/casa_default.glb');
+  casa.rotation.y = -Math.PI/2
+  casa.name = 'casa'
+  scene.add(casa)
+
+  objectRegistry.set('casa', casa)
+
+  const user: THREE.Object3D = await ModelLoader.loadGLTFSceneModel(ModelLoader.GLTF_LOADER, 'checkers/pawn_white.glb')
+  user.position.set(-0.1,0, 3.375)
+  user.scale.set(0.0625,0.0625,0.0625)
+  user.rotation.set(0, Math.PI/3, Math.PI/8)
+
+  scene.add(user)
+  objectRegistry.set('user1', user)
+}
+
+async function generateUserMember(n: number) {
+  const user: THREE.Object3D = await ModelLoader.loadGLTFSceneModel(ModelLoader.GLTF_LOADER, 'checkers/pawn_black.glb')
+  user.scale.set(0.0625,0.0625,0.0625)
+  user.rotation.set(0, Math.PI/3, Math.PI/8)
+  switch (n) {
+    case 2:
+      user.position.set(-0.4,0, 3.05);
+      break
+    case 3:
+      user.position.set(0.25,0, 2.95);
+      break
+    case 4:
+      user.position.set(0.6,0, 2.65);
+      break
+  }
+
+  user.name = 'user'+n
+  scene.add(user)
+
+  objectRegistry.set('user'+n, user)
+}
+
+function lookCasa() {
+  camera.value.position.set(0,0.3, 6.5)
+}
+
+
+function setupLights() {
+  const ambient: THREE.AmbientLight = new THREE.AmbientLight();
+  ambient.position.y = 5;
+  ambient.color.setRGB(255, 255, 255);
+  ambient.intensity = 0.005
+
+  const directional1 = new THREE.DirectionalLight( 0xffffff, 1.5)
+  directional1.position.set(8, 10, 7.5)
+  const directional2 = new THREE.DirectionalLight( 0xffffff, 1.5)
+  directional2.position.set(10, 10, 2)
+
+  const targetObject = new THREE.Object3D();
+  scene.add(targetObject);
+
+  directional2.target = targetObject
+  targetObject.position.set(0, 5, -10)
+
+  scene.add(ambient);
+  scene.add(directional1);
+  scene.add(directional2);
+
+}
+
+/**
+ *
+ *   AMIS, GROUPES & MATCHMAKING
+ *
+ */
 
 const matchmakingBanner: Ref<HTMLElement | null> = ref(null);
 const wsocial: Ref<HTMLElement | null> = ref(null);
@@ -35,7 +168,6 @@ const menuInfo = reactive({
   friendList: ([] as FriendInterface[]),
   group: (undefined as GroupInterface | undefined)
 });
-
 
 const notification = reactive({
   id: 0,
@@ -54,6 +186,9 @@ const user: Ref<UserInterface> = ref({
   createdAt: ''
 })
 
+/**
+ * Obtenir la liste d'ami et l'afficher
+ */
 async function getFriendList() {
   return AccountServices.getFriends().then((response) => {
     const _friends: FriendInterface[] = (response.data as FriendInterface[])
@@ -76,102 +211,158 @@ async function getFriendList() {
   })
 }
 
+/**
+ * Obtenir les informations du groupe et l'afficher
+ */
 function getGroupInfo() {
-  ws.emit('group_info', (error: any, response: any) => {
+  ws.emit('group_info',async (error: any, response: any) => {
+    for (let i=2; i <= 4; i++) {
+      objectRegistry.get('user'+i)?.removeFromParent()
+    }
+
+    console.log('cc', response);
+
     if (response) {
       menuInfo.group = {
         group: response.group,
         leader: response.leader,
         players: response.players as FriendInterface[]
       } as GroupInterface;
+
+      let n = 2;
+
+      for (const member of menuInfo.group.players) {
+        if (user.value.id != member.id) {
+          await generateUserMember(n)
+          n++
+        }
+      }
     } else {
       menuInfo.group = undefined;
     }
-
   })
 }
 
 function init() {
+  scene = new THREE.Scene();  // Créer la scène
 
-  AccountServices.getUserInfos().then((response) => {
-    user.value.id = response.data.id;
-    user.value.username = response.data.username;
-    user.value.points = response.data.points;
-    user.value.updatedAt = response.data.updatedAt;
-    user.value.createdAt = response.data.createdAt;
+  camera = ref(new THREE.PerspectiveCamera(25, aspectRatio.value, 0.1, 1000)); // Définie la perspective de la caméra
+  lookCasa();
 
-    ws = io(ADONIS_URL, {
-      auth: {
-        user: user.value.id,
-        token: AccountServices.getToken(),
-      }
+  onMounted(() => {
+    // configuration du renderer
+    renderer = ref(new THREE.WebGLRenderer({
+      canvas: experience.value as unknown as HTMLCanvasElement,
+      powerPreference: "high-performance",
+      antialias: false,
+    }));
+
+    renderer.value.setClearColor( 0x000000, 0 ); // the default
+
+    // Actualisation des paramètres du renderer et de la caméra
+    updateCamera();
+    updateRenderer();
+
+    setupLights();
+
+    // Lancer la boucle
+    animate();
+
+    generateCasa().then(() => {
+      AccountServices.getUserInfos().then((response) => {
+        user.value.id = response.data.id;
+        user.value.username = response.data.username;
+        user.value.points = response.data.points;
+        user.value.updatedAt = response.data.updatedAt;
+        user.value.createdAt = response.data.createdAt;
+
+        ws = io(ADONIS_URL, {
+          auth: {
+            user: user.value.id,
+            token: AccountServices.getToken(),
+          }
+        });
+
+        ws.connect();
+
+        // lorsque le socket est connecté
+        ws.on('connect', () => {
+          menuInfo.matchmaking.canStart = true;
+          getGroupInfo();
+          getFriendList().then(() => showLoader.value = false);
+        });
+
+        ws.on('reconnect', () => {
+          menuInfo.matchmaking.canStart = true;
+          getGroupInfo();
+          getFriendList().then(() => showLoader.value = false);
+        });
+
+        // lors d'une déconnexion
+        ws.on('disconnect', () => {
+          menuInfo.matchmaking.canStart = false;
+        });
+
+        // Initialisation du matchmaking
+        ws.on('matchmaking_init', (response: MatchmakingResponse) => {
+          menuInfo.matchmaking.response = response;
+          getGroupInfo();
+          if (!menuInfo.matchmaking.isInQueue) showMatchmakingBanner();
+        });
+
+        // Information du matchmaking
+        ws.on('matchmaking_info', (response: MatchmakingResponse) => {
+          menuInfo.matchmaking.response = response;
+        });
+
+        // MatchmakingError >> cancel
+        ws.on('matchmaking_error', (error: MatchmakingError) => {
+          matchmakingError(error);
+        });
+
+        // Matchmaking confirm >> lancement de la partie
+        ws.on('matchmaking_confirm', (response: MatchmakingResponse) => {
+          menuInfo.matchmaking.response = response;
+          ws.disconnect();
+          router.push('/game');
+        });
+
+        ws.on('matchmaking_leave', () => {
+          if (menuInfo.matchmaking.isInQueue) showMatchmakingBanner()
+        });
+
+        // Join lobby room (cas où le joueur membre d'un groupe)
+        // !IMPORTANT! permet au joueur membre d'un groupe de recevoir la confirmation de matchmaking
+        ws.on('lobby_join_room', () => {
+          ws.emit('lobby_join_room');
+        });
+
+        // Ping par le serveur
+        ws.on('ping', (callback) => {
+          return callback(undefined, "here")
+        });
+
+        // Lorsqu'un utilisateur nous invite
+        ws.on('group_invite', (userId: number, response: {message: string}) => {
+          showNotification(response.message, () => acceptGroupInvitation(userId), true)
+        });
+
+        ws.on('group_update', () => {
+          for (let i=2; i <= 4; i++) {
+            objectRegistry.get('user'+i)?.removeFromParent()
+          }
+          getGroupInfo();
+        });
+
+      }).catch(error => {
+        AccountServices.logout(router);
+      });
     });
 
-    ws.connect();
 
-    // lorsque le socket est connecté
-    ws.on('connect', () => {
-      menuInfo.matchmaking.canStart = true;
-      getFriendList();
-      getGroupInfo();
-      showLoader.value = false;
-    });
+  })
 
-    // lors d'une déconnexion
-    ws.on('disconnect', () => {
-      menuInfo.matchmaking.canStart = false;
-    });
 
-    // Initialisation du matchmaking
-    ws.on('matchmaking_init', (response: MatchmakingResponse) => {
-      menuInfo.matchmaking.response = response;
-      if (!menuInfo.matchmaking.isInQueue) showMatchmakingBanner();
-    });
-
-    // Information du matchmaking
-    ws.on('matchmaking_info', (response: MatchmakingResponse) => {
-      menuInfo.matchmaking.response = response;
-    });
-
-    // MatchmakingError >> cancel
-    ws.on('matchmaking_error', (error: MatchmakingError) => {
-      matchmakingError(error);
-    });
-
-    // Matchmaking confirm >> lancement de la partie
-    ws.on('matchmaking_confirm', (response: MatchmakingResponse) => {
-      menuInfo.matchmaking.response = response;
-      ws.disconnect();
-      router.push('/game');
-    });
-
-    ws.on('matchmaking_leave', () => {
-      if (menuInfo.matchmaking.isInQueue) showMatchmakingBanner()
-    });
-
-    // Join lobby room (cas où le joueur membre d'un groupe)
-    // !IMPORTANT! permet au joueur membre d'un groupe de recevoir la confirmation de matchmaking
-    ws.on('lobby_join_room', () => {
-      ws.emit('lobby_join_room');
-    });
-
-    // Ping par le serveur
-    ws.on('ping', (callback) => {
-      return callback(undefined, "here")
-    });
-
-    // Lorsqu'un utilisateur nous invite
-    ws.on('group_invite', (userId: number, response: {message: string}) => {
-      showNotification(response.message, () => acceptGroupInvitation(userId), true)
-    });
-
-    ws.on('group_update', () => {
-      getGroupInfo();
-    });
-
-  }).catch(error => {
-    AccountServices.logout(router);
-  });
 }
 
 /**
@@ -350,6 +541,8 @@ init();
 
     <!-- Contenue de la page -->
     <section class="container-section">
+
+      <canvas class="scene" ref="experience"></canvas>
       <!--
         Le contenue de la catégorie sélectionné via la barre de navigation
         sera placé dans cette section
@@ -359,7 +552,9 @@ init();
       -->
 
       <!-- Affiche de manière conditionnel un des composants -->
-      <MainMenu v-if="menuInfo.page == 'main'" :menu-info="menuInfo" :start-matchmaking="startMatchmaking"></MainMenu>
+      <MainMenu v-if="menuInfo.page == 'main'" :menu-info="menuInfo" :start-matchmaking="startMatchmaking">
+        <img src="@/scene_assets/textures/sky/casa/casa_default_sky.jpg" alt="casa default background">
+      </MainMenu>
     </section>
   </main>
 </template>
@@ -369,7 +564,7 @@ init();
   .top-section {
     height: 13vh;
     width: 100vw;
-    background: white;
+    background: linear-gradient(180deg, rgba(var(--background-color), 0.85) 0%, rgba(var(--background-color), 0) 95%);
     position: fixed;
     z-index: 2;
   }
@@ -443,6 +638,22 @@ init();
     top: 12vh;
     width: 100vw;
     position: absolute;
+  }
+
+  .container-section>.scene {
+    position: fixed;
+    top: 0;
+    z-index: 0;
+    width: 100vw;
+    height: 88vh;
+  }
+
+  .container-section>img {
+    height: 100vh;
+    width: 100vw;
+    top: 0;
+    position: fixed;
+    z-index: -1;
   }
 
   .social-widget {
