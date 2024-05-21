68a17d4dd192f583c5f857e3bfce689e3f33fb6a
Joris VILARDELL <73846641+ZUHOWKS@users.noreply.github.com>
2024-05-22 00:13:41 +0200

    Delete PlayTrays-GS/src/server.js

--- PlayTrays-GS/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PTServer_1 = require("./modules/PTServer");
-var http_1 = require("http");
-require('dotenv').config();
-var httpServer = (0, http_1.createServer)();
-var gServer = new PTServer_1.default(httpServer, 10);
-httpServer.listen(25526, '0.0.0.0');
-gServer.init();
